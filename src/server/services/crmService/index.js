22252d4caf8d0beb588d56a88870e29bd696e565
je-lopez <je.lopez@me.com>
2017-09-13 14:58:23 -0700

    moves notifyContactSignedUp from index to its own file

--- src/server/services/crmService/index.js
+++ src/server/services/crmService/index.js
@@ -1,15 +1,5 @@
-import fetch from 'isomorphic-fetch'
-import config from 'src/config'
 import {default as getContactByEmail} from './getContactByEmail'
-
-const properties = {
-  echoSignUp: 'signed_up_for_echo',
-}
-
-const paths = {
-  contactProfileByEmail: email => `/contacts/v1/contact/email/${email}/profile`,
-  contactProfileByVID: vid => `/contacts/v1/contact/vid/${vid}/profile`,
-}
+import {default as notifyContactSignedUp} from './notifyContactSignedUp'
 
 /**
  * NOTE: this service's functions are exported the way they are to enable
@@ -19,44 +9,4 @@ const paths = {
 export default {
   getContactByEmail,
   notifyContactSignedUp,
-}
-
-async function notifyContactSignedUp(email) {
-  const contact = await getContactByEmail(email)
-
-  const url = _crmURL(paths.contactProfileByVID(contact.vid))
-  const resp = await fetch(url, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application.json'
-    },
-    body: JSON.stringify({
-      properties: [{
-        property: properties.echoSignUp,
-        value: true,
-      }]
-    })
-  })
-
-  if (!resp.ok) {
-    throw new Error(`Couldn't notify that contact signed up: ${resp.statusText}`)
-  }
-
-  // API returns statusCode 204 with no body
-  return true
-}
-
-function _crmURL(path) {
-  _assertEnvironment()
-  return `${config.server.crm.baseURL}${path}?hapikey=${config.server.crm.key}`
-}
-
-function _assertEnvironment() {
-  if (!config.server.crm.baseURL) {
-    throw new Error('CRM base URL must be configured')
-  }
-  if (!config.server.crm.key) {
-    throw new Error('CRM API key must be configured')
-  }
 }
